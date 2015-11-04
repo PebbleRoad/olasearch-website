6f5a03eb35723fe9236e7575b166551a362b02e3
Vinay <rmdort@gmail.com>
2015-11-04 23:04:53 +0800

    updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-$(function(){
+;$(function(){
 
 	/**
 	 * Material fields
@@ -17,6 +17,50 @@ $(function(){
 
 	$body.on('click', '.ola-nav a', function(e){
 		$body.removeClass('menu-opened');
+	});
+
+	
+
+	function scrollTo(element, to, duration) {
+	    var start = element.scrollTop,
+	        change = to - start,
+	        increment = 20;
+
+	    var animateScroll = function(elapsedTime) {        
+	        elapsedTime += increment;
+	        var position = easeInOut(elapsedTime, start, change, duration);                        
+	        element.scrollTop = position; 
+	        if (elapsedTime < duration) {
+	            setTimeout(function() {
+	                animateScroll(elapsedTime);
+	            }, increment);
+	        }
+	    };
+
+	    animateScroll(0);
+	}
+
+	function easeInOut(currentTime, start, change, duration) {
+	    currentTime /= duration / 2;
+	    if (currentTime < 1) {
+	        return change / 2 * currentTime * currentTime + start;
+	    }
+	    currentTime -= 1;
+	    return -change / 2 * (currentTime * (currentTime - 2) - 1) + start;
+	}
+
+	/**
+	 * Navigation click
+	 */
+	
+	$body.on('click.nav', '.ola-nav a', function(e){
+
+		var id = $(this).attr('href');
+
+		var top = $(id).offset().top		
+		scrollTo(document.body, top , 500)
+
+		e.preventDefault();
 	});
 
 
